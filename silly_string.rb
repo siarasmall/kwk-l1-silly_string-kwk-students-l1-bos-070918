640ecf844d7da5736b407635dcfa27afeecc6458
siarasmall <siarasmall19@gmail.com>
2018-07-09 19:16:18 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -41,9 +41,9 @@ proper_sentence = "i really like programming.".capitalize # Put a . after the st
 puts proper_sentence
 
 # Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("a","o").gsub("A","O")
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
 
-puts sentence.gsub("a","o").gsub("A","O")
+puts sentence.
